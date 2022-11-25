e8acea3f51599d75eb7564ea36527be83edd37a1
cristianh <crishernandez08@hotmail.com>
2022-11-25 13:31:19 -0500

    update: agregando market personalizado

---
+++ dist/public/rutas.js
@@ -0,0 +1,38 @@
+const geojson = {
+  type: 'FeatureCollection',
+  features: [
+    {
+      type: 'Feature',
+      geometry: {
+        type: 'Point',
+        coordinates: [-75.6808835, 4.5322931]
+      },
+      properties: {
+        title: 'Ruta 18',
+        description: 'Sur/Norte'
+      }
+    },
+    {
+      type: 'Feature',
+      geometry: {
+        type: 'Point',
+        coordinates: [-75.6794308, 4.5309642]
+      },
+      properties: {
+        title: 'Ruta 18',
+        description: 'Norte/Sur'
+      }
+    },
+    {
+      type: 'Feature',
+      geometry: {
+        type: 'Point',
+        coordinates: [-75.6692804, 4.5408573]
+      },
+      properties: {
+        title: 'Ruta 20',
+        description: 'Norte/Sur'
+      }
+    }
+  ]
+};
