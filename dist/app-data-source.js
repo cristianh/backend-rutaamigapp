8717db9f496c31db1579594f4428f6ce7687b07f
cristianh <crishernandez08@hotmail.com>
2023-04-12 09:23:55 -0500

    editando la variable url de la conexion (no va)

--- dist/app-data-source.js
+++ dist/app-data-source.js
@@ -9,13 +9,28 @@ dotenv.config();
     NOTA: REVISAR EL ARCHIVO .env SI DEBEN REALIZAR ALGUN CAMBIO
     OJO!: REVISEN QUE EL ARCHIVO CON SU CONFIGURACION NO SE SUBA AL REPO.(YA ESTA EN EL ARCHIVO GIFIGNORE)
 */
-var myDataSource = new typeorm_1.DataSource({
+/* const myDataSource = new DataSource({
     type: "mysql",
+    url:process.env.DB_URL_POSTGRESSQL,
     host: process.env.DBHOST,
     port: parseInt(process.env.DBPORT),
     username: process.env.DBUSER,
     password: process.env.DBPASSWORD,
     database: process.env.DBNAME,
+    entities: [__dirname + "/src/entity/*.js"],
+    logging: false,
+    synchronize: true,
+}) */
+/* postgres-conexion - REMOTE
+*/
+var myDataSource = new typeorm_1.DataSource({
+    type: "postgres",
+    url: process.env.DBURLREMOTE,
+    port: parseInt(process.env.DBPORTREMOTE),
+    username: process.env.DBUSERREMOTE,
+    password: process.env.DBPASSWORDREMOTE,
+    database: process.env.DBNAMEREMOTE,
+    ssl: { rejectUnauthorized: false },
     entities: [__dirname + "/src/entity/*.js"],
     logging: false,
     synchronize: true,
