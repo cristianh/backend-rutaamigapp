9ba83efe5051f9d34fae00c2d73a7d59741a28ae
cristianh <crishernandez08@hotmail.com>
2023-04-22 21:29:09 -0500

    Rebase-Revert "UPDATE: actualizando e integrando controlador router y user"
    This reverts commit 52522ea88c3206ba28ec2f51590276ae9af89f1d.

--- src/controllers/file.controller.ts
+++ src/controllers/file.controller.ts
@@ -56,7 +56,7 @@ export class FileController {
 
             const dbUserFile = new File();
             dbUserFile.file_name = filename,
-                dbUserFile.cloudinary_url = cloudinary_url
+            dbUserFile.cloudinary_url = cloudinary_url
             dbUserFile.user = user
             const fileuser = await myDataSource.getRepository(File).save(dbUserFile)
             return res.status(201).send({ status: "¡Archivo cargado correctamente!", File })
