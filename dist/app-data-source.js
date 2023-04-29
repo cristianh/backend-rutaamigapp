48e81d4fe27a1cf9dc9823a008b32a91f413f1ea
Tu nombre <jessica.berrio2104@gmail.com>
2023-04-29 10:48:25 -0500

    Configuracion datasource

--- dist/app-data-source.js
+++ dist/app-data-source.js
@@ -9,22 +9,22 @@ dotenv.config();
     NOTA: REVISAR EL ARCHIVO .env SI DEBEN REALIZAR ALGUN CAMBIO
     OJO!: REVISEN QUE EL ARCHIVO CON SU CONFIGURACION NO SE SUBA AL REPO.(YA ESTA EN EL ARCHIVO GIFIGNORE)
 */
-/* const myDataSource = new DataSource({
+var myDataSource = new typeorm_1.DataSource({
     type: "mysql",
     host: process.env.DBHOST,
     port: parseInt(process.env.DBPORT),
     username: process.env.DBUSER,
     password: process.env.DBPASSWORD,
     database: process.env.DBNAME,
-    entities: [__dirname + "./../entity/*.js"],
+    entities: [__dirname + "/src/entity/*.js"],
     logging: false,
     synchronize: true,
-}) */
+});
 /* postgres-conexion - REMOTE
 */
-var myDataSource = new typeorm_1.DataSource({
+/* const myDataSource = new DataSource({
     type: "postgres",
-    url: process.env.DBURLREMOTE,
+    url:process.env.DBURLREMOTE,
     port: parseInt(process.env.DBPORTREMOTE),
     username: process.env.DBUSERREMOTE,
     password: process.env.DBPASSWORDREMOTE,
@@ -33,6 +33,6 @@ var myDataSource = new typeorm_1.DataSource({
     entities: [__dirname + "/src/entity/*.js"],
     logging: false,
     synchronize: true,
-});
+}) */
 exports.default = myDataSource;
 //# sourceMappingURL=app-data-source.js.map
