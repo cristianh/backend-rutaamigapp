81f725fcd4d2e2b4f995788b04f6c351083fb6b0
cristianh <crishernandez08@hotmail.com>
2023-05-26 14:08:23 -0500

    ajustando conexion

--- dist/src/app-data-source.js
+++ dist/src/app-data-source.js
@@ -18,30 +18,31 @@ var port = typeof dbPort === 'string' ? parseInt(dbPort) : undefined;
     NOTA: REVISAR EL ARCHIVO .env SI DEBEN REALIZAR ALGUN CAMBIO
     OJO!: REVISEN QUE EL ARCHIVO CON SU CONFIGURACION NO SE SUBA AL REPO.(YA ESTA EN EL ARCHIVO GIFIGNORE)
 */
-/* const myDataSource = new DataSource({
+var myDataSource = new typeorm_1.DataSource({
     type: "mysql",
     host: process.env.DBHOST,
     port: port,
     username: process.env.DBUSER,
     password: process.env.DBPASSWORD,
     database: process.env.DBNAME,
-    entities: [User,File,Notification,Rol,Route],
+    entities: [user_entity_1.User, file_entity_1.File, notification_entity_1.Notification, rol_entity_1.Rol, route_entity_1.Route],
     logging: false,
     synchronize: false,
-}) */
+});
 /* postgres-conexion - REMOTE
 */
-var myDataSource = new typeorm_1.DataSource({
+/* const myDataSource = new DataSource({
     type: "postgres",
-    url: process.env.DBURLREMOTE,
+    url:process.env.DBURLREMOTE,
     port: parseInt(process.env.DBPORTREMOTE),
     username: process.env.DBUSERREMOTE,
     password: process.env.DBPASSWORDREMOTE,
     database: process.env.DBNAMEREMOTE,
     ssl: { rejectUnauthorized: false },
-    entities: [user_entity_1.User, file_entity_1.File, notification_entity_1.Notification, rol_entity_1.Rol, route_entity_1.Route],
+    entities: [User,File,Notification,Rol,Route],//cambiar por el de arriba al subir.
     logging: false,
     synchronize: true,
-});
+})
+ */
 exports.default = myDataSource;
 //# sourceMappingURL=app-data-source.js.map
